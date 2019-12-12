3600feac970da185dd8a9f6eb46e12fd6e089dfc
theSunshine666 <34849039+theSunshine666@users.noreply.github.com>
2019-12-12 23:55:42 +0800

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,23 @@
+package com.wq.gppm.bean;
+
+public class Admin {
+    private String adminName;
+
+    private String adminPassword;
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName == null ? null : adminName.trim();
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword == null ? null : adminPassword.trim();
+    }
+}
